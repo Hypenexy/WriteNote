001d96f3c21b2905bad7f00ec0e9601eec1a24a7
Hypenexy <61563710+Hypenexy@users.noreply.github.com>
2020-12-06 01:08:29 +0200

    The game

---
+++ Snake/Snake/Piece.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    class Piece : Label
+    {
+        public Piece(int x, int y)
+        {
+            Location = new Point(x, y);
+            Size = new Size(20, 20);
+            Size = new Size(20, 20);
+            BackColor = Color.BlueViolet;
+            StreamReader sr = new StreamReader("Theme.WRProp");
+            var checkTheme = sr.ReadToEnd();
+            string s1 = "light";
+            bool b = checkTheme.Contains(s1);
+            sr.Close();
+            if (b == true)
+            {
+                BackColor = Color.Gold;
+            }
+            //Image = Image.FromFile("C:/Users/hypen/source/repos/Snake/Snake/Resources/writenotebg.png");
+            Enabled = false;
+        }
+    }
+}
