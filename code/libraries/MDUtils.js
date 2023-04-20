a1ba27ccd3ad14b9fb04afd76561d5e2318f2638
Midelight <61563710+Hypenexy@users.noreply.github.com>
2023-04-20 16:10:09 +0300

    removed Mobile, updated guidGenerator

--- code/libraries/MDUtils.js
+++ code/libraries/MDUtils.js
@@ -576,13 +576,18 @@ function formatPlaybackTime(seconds) {
 
 /**
  * Used to generate random Ids
+ * @param {Number} length Custom length to return if null it's 36
  * @returns A random string
  */
-function guidGenerator(){
-    var S4 = function() {
-       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    };
-    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+function guidGenerator(length){
+    var S4 = function(){
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1)
+    }
+    var guid = S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4()
+    if(length){
+        return guid.slice(0, length)
+    }
+    return guid
 }
 
 /**
