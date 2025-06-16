5cf30ddcc875d2197c790ff8e96c60cebf3fdcb5
Hypenexy <61563710+Hypenexy@users.noreply.github.com>
2025-06-17 01:32:00 +0300

    Render improvements

--- App/code/writenote/editor/index.js
+++ App/code/writenote/editor/index.js
@@ -43,84 +43,10 @@ function InitializeEditor(state) {
     data.push({ id: 2, segments: [{ t: "Line 2: Another line" }] });
     data.push({ id: 3, segments: [{ img: "./assets/images/untitled5-16.png" }] });
     data.push({ id: 4, segments: [{ t: "Custom color li", s: "c", c: "f00000" }, { t: "ne", s: "c", c: "00ff00" }] });
+    data.push({ id: 5, segments: [{ t: "Line 5: A line with a very long text that should wrap around to the next line if it is too long." }] });
+    
+    initializeRender(notearea, carets, cursors, data);
 
-    function renderEditor() {
-        notearea.innerHTML = "";
-    
-        for (let i = 1; i < data.length; i++) {
-            const line = data[i];
-
-            const div = document.createElement("div");
-            div.classList.add("line");
-            div.setAttribute("data-id", line.id);
-    
-            line.segments.forEach(segment => {
-                let elem = segment.img ? document.createElement("img") : document.createElement("span");
-    
-                if (segment.img) {
-                    elem.src = segment.img;
-                } else {
-                    elem.textContent = segment.t;
-                    if(segment.s){
-                        applyStyles(elem, segment.s, segment.c);
-                    }
-                }
-    
-                div.appendChild(elem);
-            });
-    
-            notearea.appendChild(div);
-        }
-
-        
-        carets.innerHTML = "";
-        for (let i = 0; i < cursors.length; i++) {
-            var caret = document.createElement("div");
-            caret.classList.add("caret");
-            var line = notearea.children[cursors[i].line - 1];
-            var top = line.getBoundingClientRect().top;
-            var left = new TextMeasurer().getWidthOfNCharacters(line, cursors[i].start);
-            
-            caret.style.left = left + "px";
-            caret.style.top = top + "px";
-            // Check if the caret should be placed next to an image segment
-            if (line) {
-                let charIndex = cursors[i].start;
-                let segments = data[cursors[i].line]?.segments || [];
-                let runningLength = 0;
-                for (let segIdx = 0; segIdx < segments.length; segIdx++) {
-                    let seg = segments[segIdx];
-                    let segLength = seg.img ? 1 : (seg.t ? seg.t.length : 0);
-                    if (charIndex <= runningLength + segLength) {
-                        if (seg.img) {
-                            // Place caret just after the image and match its height
-                            const imgs = line.querySelectorAll("img");
-                            const img = imgs[segIdx]; // Match segment index
-                            if (img) {
-                                const rect = img.getBoundingClientRect();
-                                const lineRect = line.getBoundingClientRect();
-                                caret.style.left = (rect.right - lineRect.left) + "px";
-                                caret.style.top = (rect.top - lineRect.top) + "px";
-                                caret.style.height = rect.height + "px";
-                                caret.style.width = "2px";
-                                // caret.style.background = "black";
-                            }
-                        }
-                        break;
-                    }
-                    runningLength += segLength;
-                }
-            }
-
-            carets.appendChild(caret);
-            
-            console.log(cursors[i]);
-            
-        }
-    }
-    
-    renderEditor();
-    
     function getLineRowIndexes(event){
         var clickedLine = mdutils.findElement(event.target, ".line");
         if(!clickedLine){
---
+++ App/code/writenote/editor/utils/Cursors.js
@@ -0,0 +1,46 @@
+function renderCarets(carets, cursors, notearea, data) {
+    carets.innerHTML = "";
+    for (let i = 0; i < cursors.length; i++) {
+        var caret = document.createElement("div");
+        caret.classList.add("caret");
+        var line = notearea.children[cursors[i].line - 1];
+        var top = line.getBoundingClientRect().top;
+        var left = new TextMeasurer().getWidthOfNCharacters(line, cursors[i].start);
+        
+        caret.style.left = left + "px";
+        caret.style.top = top + "px";
+        // Check if the caret should be placed next to an image segment
+        if (line) {
+            let charIndex = cursors[i].start;
+            let segments = data[cursors[i].line]?.segments || [];
+            let runningLength = 0;
+            for (let segIdx = 0; segIdx < segments.length; segIdx++) {
+                let seg = segments[segIdx];
+                let segLength = seg.img ? 1 : (seg.t ? seg.t.length : 0);
+                if (charIndex <= runningLength + segLength) {
+                    if (seg.img) {
+                        // Place caret just after the image and match its height
+                        const imgs = line.querySelectorAll("img");
+                        const img = imgs[segIdx]; // Match segment index
+                        if (img) {
+                            const rect = img.getBoundingClientRect();
+                            const lineRect = line.getBoundingClientRect();
+                            caret.style.left = (rect.right - lineRect.left) + "px";
+                            caret.style.top = (rect.top - lineRect.top) + "px";
+                            caret.style.height = rect.height + "px";
+                            caret.style.width = "2px";
+                            // caret.style.background = "black";
+                        }
+                    }
+                    break;
+                }
+                runningLength += segLength;
+            }
+        }
+
+        carets.appendChild(caret);
+        
+        console.log(cursors[i]);
+        console.log(cursors[i]);
+    }
+}
